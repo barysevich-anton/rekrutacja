f21aab83d45de4c017f7aff8926c95919d126d1c
Anton <80327025+barysevich-anton@users.noreply.github.com>
2022-04-22 18:57:59 +0200

    Update json.js

--- js/json.js
+++ js/json.js
@@ -1,17 +1,32 @@
-function updateDiv(){
+const sliderBlock = document.getElementById('result')
 
-    $.getJSON('swiper.json', function(data){
-    
-                var i=Math.floor(Math.random()*data.length);
+fetch('https://quantoo.pl/jobs/api/swiper/list.json')
+    .then((res) => res.json())
+    .then(data => setObj(data))
+
+    function setObj({items}){
+        document.body.insertAdjacentHTML('afterend', `
+        <div class="swiper mySwiper">
+            <div class="swiper-wrapper">
+                ${setItems(items)}
+            </div>
+            <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>
+      <div class="swiper-pagination"></div>
+         </div>
             
-                    
-                    $('#result').append('<img src= "'+data[i].img+'" class="slider-img">');	
-                        $('#result').append('<div class="title">'+'First Name:'+data[i].name+'</div>');
-                        $('#result').append('<div class="content">'+'Last  Name:'+data[i].lead+'</div>');
-                $('#result').append('<div class="price">'+'Email  Id:'+data[i].price+'</div>');
-                    
-                    
-                        });
- 
-         }
- 
+        `)
+    }
+
+function setItems(items){
+    return items.map(
+        slide => `
+        <div class="swiper-slide">
+            <img src="${slide.img}" class="img">
+            <h1 class="title">${slide.name}</h1>
+            <p class="lead">${slide.lead}</p>
+            <h3 class="price">${slide.price}</h3>
+            </div>
+        `)
+        .join(' ')
+}
